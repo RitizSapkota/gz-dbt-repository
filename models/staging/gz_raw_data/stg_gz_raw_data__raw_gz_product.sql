1c6fe75911eceecbb2bf38ccdf0094cfd7154b7a
ritizsapkota15@gmail.com <ritizsapkota15@gmail.com>
2024-12-10 16:58:17 +0000

    basic transformations

---
+++ models/staging/gz_raw_data/stg_gz_raw_data__raw_gz_product.sql
@@ -0,0 +1,20 @@
+with 
+
+source as (
+
+    select * from {{ source('gz_raw_data', 'raw_gz_product') }}
+
+),
+
+renamed as (
+
+    select
+        products_id,
+        purchse_price as purchase_price,
+        cast(purchse_price as FLOAT64) as purchase_price
+
+    from source
+
+)
+
+select * from renamed
